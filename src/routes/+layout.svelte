1f3641a9ff97593e71d391f8240372b62a2e7e75
Stephen Wilde <kink@mandys.house>
2023-02-10 10:46:30 +0000

    add simple theme switch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,29 @@
 <script>
     import '../app.css';
+
+    import { onMount } from 'svelte';
+
+    onMount(async () => {
+        var d = document.documentElement,
+            t = document.querySelector('#theme-btn'),
+            m = localStorage.getItem('site-theme');
+
+        if (m == 'dark') {
+            d.classList.add('theme-dark');
+        }
+
+        t.addEventListener('click', function () {
+            if (d.classList.contains('tw-dark')) {
+                d.classList.remove('tw-dark');
+                localStorage.removeItem('site-theme');
+            } else {
+                d.classList.add('tw-dark');
+                localStorage.setItem('site-theme', 'dark');
+            }
+        });
+    });
 </script>
 
 <slot />
+
+<button class="" id="theme-btn" type="button">Swap Theme</button>
