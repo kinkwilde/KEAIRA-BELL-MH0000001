d345349b5d61c30d8d4fffbf8e355bf375f8351f
Stephen Wilde <kink@mandys.house>
2023-02-13 14:20:42 +0000

    add example pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,7 @@
+<script>
+    import PageHero from '$lib/components/contents/Hero.svelte';
+</script>
+
 <div class="cm-section">
-    <div class="tw-container">
-        <div class="tw-mx-auto tw-flex tw-flex-col tw-items-center sm:tw-w-2/3">
-            <h1
-                class="tw-mb-0 tw-text-center tw-text-2xl sm:tw-text-3xl md:tw-text-4xl lg:tw-text-5xl xl:tw-text-6xl"
-            >
-                Homepage
-            </h1>
-        </div>
-    </div>
+    <PageHero linkOneLink="/about" linkTwoLink="/blog" />
 </div>
